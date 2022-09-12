7c922fbac7e4c592ce518f1adc56e498f14ef16d
trainingtuts <trainingtuts@gmail.com>
2022-09-12 13:55:50 +0500

    firebase integrated for ios, android

--- vendor_app/lib/main.dart
+++ vendor_app/lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:vendor_app/firebase_options.dart';
 import 'package:vendor_app/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
